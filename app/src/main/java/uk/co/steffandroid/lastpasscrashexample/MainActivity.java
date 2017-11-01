cbff5c46e1dfa20fef4d1ba0cf16fc64b1065cde
Steffan Davies <steff@steffandroid.co.uk>
2017-11-01 03:43:56 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/steffandroid/lastpasscrashexample/MainActivity.java
@@ -0,0 +1,19 @@
+package uk.co.steffandroid.lastpasscrashexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.forgot_password);
+        Linkify.addLinks(textView, Pattern.compile("^*.*$"), "https://my.vanmoof.com/password/email/", null, (matcher, s) -> "");
+    }
+}
